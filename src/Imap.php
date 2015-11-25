b3f8b14177cd5a0e70ad9b08bb17246eae9cd600
Rudys Natanael Acosta <natanael926@gmail.com>
2015-11-25 08:53:18 -0400

    Agragr src

---
+++ src/Imap.php
@@ -0,0 +1,15 @@
+<?php namespace MailImap;
+
+/**
+ *
+ * @package MailImap
+ * @author  Rudys Natanael Acosta <natanael926@gmail.com>
+ */
+class Imap {
+
+	public function __construct()
+	{
+		return 'Prueba aaa';
+	}
+
+}
